a82b5ac5220166f1d65c887e7e82e393f892382f
Enzo Conty <contact@enzoconty.dev>
2022-12-01 11:43:11 +0100

    ✨ Day 1

--- 2022/solutions/day01.dart
+++ 2022/solutions/day01.dart
@@ -1,38 +1,46 @@
 import '../utils/index.dart';
 
-/// Every day should extend [GenericDay] to have access to the corresponding
-/// input and a common interface.
-///
-/// Naming convention is set to pad any single-digit day with `0` to have proper
-/// ordering of files and correct mapping between input for days and the day files.
 class Day01 extends GenericDay {
-  // call the superclass with an integer == today´s day
   Day01() : super(1);
 
-  /// The [InputUtil] can be accessed through the superclass variable `input`. \
-  /// There are several methods in that class that parse the input in different
-  /// ways, an example is given below
-  ///
-  /// The return type of this is `dynamic` for [GenericDay], so you can decide
-  /// on a day-to-day basis what this function should return.
   @override
-  List<int> parseInput() {
-    final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
-  }
+  parseInput() {}
 
-  /// The `solvePartX` methods always return a int, the puzzle solution. This
-  /// solution will be printed in main.
   @override
   int solvePart1() {
-    // TODO implement
-    return 0;
+    int caloryPerItem = 0;
+    int caloryPerElf = 0;
+
+    for (var element in input.getPerLine()) {
+      if (element.isEmpty) {
+        if (caloryPerItem > caloryPerElf) {
+          caloryPerElf = caloryPerItem;
+        }
+        caloryPerItem = 0;
+      } else {
+        caloryPerItem += int.parse(element);
+      }
+    }
+
+    return caloryPerElf;
   }
 
   @override
   int solvePart2() {
-    // TODO implement
-    return 0;
+    int caloryPerItem = 0;
+    List<int> caloryPerElf = [];
+
+    for (var element in input.getPerLine()) {
+      if (element.isEmpty) {
+        caloryPerElf.add(caloryPerItem);
+        caloryPerItem = 0;
+      } else {
+        caloryPerItem += int.parse(element);
+      }
+    }
+    caloryPerElf.sort();
+    return caloryPerElf.reversed
+        .take(3)
+        .reduce((value, element) => value + element);
   }
 }
